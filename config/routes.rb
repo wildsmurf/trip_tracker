be0668daba6b1d5bbbd2c9c86b9920d9f25dfab2
Dylan Pople <dylanpople@Dylans-MacBook-Pro.local>
2017-09-01 15:54:33 -0700

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+
+  get 'addresses/show'
+
+  get 'addresses/new'
+
+  get 'locations/index'
+
+  get 'locations/show'
+
+  get 'locations/new'
+
+  get 'trips/index'
+
+  get 'trips/show'
+
+  get 'trips/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
