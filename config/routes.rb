6eaa12869e38cae68cec70dc2a53c9600eaf5c6c
Erik McComb <erik@erikmccomb.com>
2017-09-03 13:59:51 -0600

    Commiting most recent changed for TA assist

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'new_trip_address/:id', to: 'trips#new_address', as: 'new_trip_address'
   post 'add_trip_address/:id/:address_id', to: 'trips#add_address', as: 'add_trip_address'
-  get 'delete_trip_address/:id/:address_id', to: 'tripss#delete_address', as: 'delete_trip_address'
+  get 'delete_trip_address/:id/:address_id', to: 'trips#delete_address', as: 'delete_trip_address'
 
 end
 
